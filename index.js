10ccc4a2b0bc4d7eda8a20a73cb90e88c2e47e44
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-02 16:45:11 +0200

    Add let & const

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-true
-45
-12.1212
+/*
+починається з букви, $ або _
+не використовувати пробіл
+регистрозалежне
+відповідає на питання що в ній
+*/
+
+let userName = "Brad";
+// console.log(userName);
+
+let userAge; //оголошення
+userAge = 59; //ініціалізація
+
+//рік народження користувача
+const userYearOfBirth = 1963;
+
+// console.log(userName);
+// console.log(userAge);
+
+userAge = 60;
+userName = "Bradly";
+
+const userGender = "male";
+//userGender = 'female'; // error
+
+const AMOUNT_DAYS_WEEK = 7;
+
+let number = 12;
+
+{
+  let number = 89;
+  console.log(number);
+}
+
+console.log(number);
