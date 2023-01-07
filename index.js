9c9ff89065b348fa73b2c9067f0083add28de635
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-07 12:38:41 +0200

    Add compareTwoNumbers

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
 //напишіть функцію, яка приймає два числа
-//якщо числа парні - повертає суму
-//якщо непарні - повертає результат множення
-//якщо різні - повертає непарне
+//і порівнює ці числа так:
+//якщо перше більше другого - повертає 1
+//якщо друге більше першого - повертає -1
+//якщо числа однакові - повертає 0
 
-/**
- *
- * @param {number} number1
- * @param {number} number2
- * @returns {number}
- */
-const getResult = function (number1, number2) {
-  //якщо числа парні - повертає суму
-  if (number1 % 2 === 0 && number2 % 2 === 0) {
-    return number1 + number2;
+// const compareTwoNumbers = function (number1, number2) {
+//   if (number1 > number2) {
+//     return 1;
+//   }
+//   if (number1 < number2) {
+//     return -1;
+//   }
+//   return 0;
+// };
+
+const compareTwoNumbers = function (number1, number2) {
+  if (number1 === number2) {
+    return 0;
   }
-  //якщо непарні - повертає результат множення
-  if (number1 % 2 && number2 % 2) {
-    return number1 * number2;
+  if (number1 > number2) {
+    return 1;
   }
-  //якщо різні - повертає непарне
-  if (number1 % 2) {
-    return number1;
-  }
-  return number2;
+  return -1;
 };
-
-// const number1 = 5;
-// const number2 = 3;
-// let result;
-// //debugger
-// if (number1 % 2 === 0 && number2 % 2 === 0) {
-//   result = number1 + number2;
-// } else if (number1 % 2 && number2 % 2) {
-//   result = number1 * number2;
-// } else if (number1 % 2) {
-//   result = number1;
-// }else{
-//   result = number2;
-// }
