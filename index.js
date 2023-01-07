ed4b24a5b1b5a06846f1262445254fd1e2e4e0db
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-07 13:16:23 +0200

    Add calculateSquareEquation

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-//напишіть функцію, яка приймає два числа
-//і порівнює ці числа так:
-//якщо перше більше другого - повертає 1
-//якщо друге більше першого - повертає -1
-//якщо числа однакові - повертає 0
+//напишіть функцію, яка обчислює квадратне рівняння
 
-// const compareTwoNumbers = function (number1, number2) {
-//   if (number1 > number2) {
-//     return 1;
-//   }
-//   if (number1 < number2) {
-//     return -1;
-//   }
-//   return 0;
-// };
-
-const compareTwoNumbers = function (number1, number2) {
-  if (number1 === number2) {
-    return 0;
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @returns {number[] | null}
+ */
+const calculateSquareEquation = function (a = 1, b = -26, c = 120) {
+  const D = b * b - 4 * a * c;
+  if (D < 0) {
+    return null;
   }
-  if (number1 > number2) {
-    return 1;
+  if (D === 0) {
+    const x = -b / (2 * a);
+    return [x];
   }
-  return -1;
+  const x1 = (-b + Math.sqrt(D)) / (2 * a);
+  const x2 = (-b - Math.sqrt(D)) / (2 * a);
+  return [x1, x2];
 };
+
+const result = calculateSquareEquation();
+if (result) {
+  console.log(result)
+}else{
+  console.log('roots not found')
+}
