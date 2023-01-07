607bcf26eb07981556f618bcf50cd7e63c2d7034
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-07 11:32:38 +0200

    Add isMultiplicity

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-//напишіть функцію, яка буде запитувати у користувача будь що
-//і повертати значення, якщо воно було введено
-//або повертати null, якщо користувач нічого не ввів
+//напишіть функцію, яка приймає два числа (корректні)
+//і повертає кратність першого другим
 
+const number1 = 45;
+const number2 = 9;
 /**
- * 
- * @returns {string | null}
+ *
+ * @param {number} number1
+ * @param {number} number2
+ * @returns {boolean}
  */
-const getUserInput = function () {
-  const input = prompt();
-  if(input){
-    return input;
-  }
-  return null;
+const isMultiplicity = function (number1, number2) {
+  return number1 % number2 === 0;
 };
+
+if (Number.isInteger(number1) && Number.isInteger(number2)) {
+  console.log(isMultiplicity(number1, number2));
+} else {
+  console.log("number must be integer");
+}
