980ee5f5daacdbe31be4daf6f7d9be1a134963ca
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-09 16:28:44 +0200

    Add example switch

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const userInput = prompt("Choose language:\n1 - ua\n2 - en\n3 - pl\n4 - fr");
 
-// if (userInput === "1" || userInput === "ua") {
-//   console.log("привіт");
-// } else if (userInput === "2" || userInput === "en") {
-//   console.log("hi");
-// } else if (userInput === "3" || userInput === "pl") {
-//   console.log("czesc");
-// } else if (userInput === "4" || userInput === "fr") {
-//   console.log("hé");
-// } else {
-//   console.log("lang not found");
-// }
+if (userInput === "1" || userInput === "ua") {
+  console.log("привіт");
+} else if (userInput === "2" || userInput === "en") {
+  console.log("hi");
+} else if (userInput === "3" || userInput === "pl") {
+  console.log("czesc");
+} else if (userInput === "4" || userInput === "fr") {
+  console.log("hé");
+} else {
+  console.log("lang not found");
+}
 
 switch (userInput) {
   case "1":
@@ -31,5 +31,30 @@ switch (userInput) {
     break;
   default:
     console.log("lang not found");
+    break;
+}
+
+const userInput2 = prompt();
+switch (userInput2) {
+  case "1":
+  case "2":
+  case "12":
+    console.log("winter");
+    break;
+  case "3":
+  case "4":
+  case "5":
+    console.log("spring");
+    break;
+  case "6":
+  case "7":
+  case "8":
+    console.log("summer");
+    break;
+  case "9":
+  case "10":
+  case "11":
+  default:
+    console.log("error");
     break;
 }
