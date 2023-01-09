6823855e32f1cb558574fc27b864fd1a1ca99b03
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-09 17:08:53 +0200

    Add hw

--- index.js
+++ index.js
@@ -1,60 +1,31 @@
-const userInput = prompt("Choose language:\n1 - ua\n2 - en\n3 - pl\n4 - fr");
-
-if (userInput === "1" || userInput === "ua") {
-  console.log("привіт");
-} else if (userInput === "2" || userInput === "en") {
-  console.log("hi");
-} else if (userInput === "3" || userInput === "pl") {
-  console.log("czesc");
-} else if (userInput === "4" || userInput === "fr") {
-  console.log("hé");
-} else {
-  console.log("lang not found");
+const userInputAge = prompt("Enter age");
+const userAge = Number(userInputAge);
+if (
+  userInputAge === null ||
+  userInputAge === "" ||
+  Number.isNaN(userAge) ||
+  userAge < 0 ||
+  userAge >= 150
+) {
+  console.log("error"); 
+} else if(userAge<2){   //[0-150)
+  console.log("baby");
+} else if(userAge<6){   //[2-150)
+  console.log("kinder");
+}else if(userAge<12){   //[6-150)
+  console.log("child");
+}else if(userAge<18){   //[12-150)
+  console.log("tinager");
+}else if(userAge<65){   //[18-150)
+  console.log("adult");
+}else if(userAge<100){   //[65-150)
+  console.log("old");
+}else {   //[100-150)  if(userAge<150)
+  console.log("wow!!!");
 }
 
-switch (userInput) {
-  case "1":
-  case "ua":
-    console.log("привіт");
-    break;
-  case "2":
-  case "en":
-    console.log("hi");
-    break;
-  case "3":
-  case "pl":
-    console.log("czesc");
-    break;
-  case "4":
-  case "fr":
-    console.log("hé");
-    break;
-  default:
-    console.log("lang not found");
-    break;
-}
 
-const userInput2 = prompt();
-switch (userInput2) {
-  case "1":
-  case "2":
-  case "12":
-    console.log("winter");
-    break;
-  case "3":
-  case "4":
-  case "5":
-    console.log("spring");
-    break;
-  case "6":
-  case "7":
-  case "8":
-    console.log("summer");
-    break;
-  case "9":
-  case "10":
-  case "11":
-  default:
-    console.log("error");
-    break;
-}
+//hw
+// користувач вводить число, яке є номером дня місяця [1-31]
+// ви виводите назву дня тижня,
+// при у мові, що місяць почався з понеділка
