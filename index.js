07e8b4f3fb306c47047405a494a9b0ccffd6c437
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-10 14:59:31 +0200

    Add clear function & hight order function (HOF)

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
-const userInputAge = prompt("Enter age");
-const userAge = Number(userInputAge);
-if (
-  userInputAge === null ||
-  userInputAge === "" ||
-  Number.isNaN(userAge) ||
-  userAge < 0 ||
-  userAge >= 150
-) {
-  console.log("error"); 
-} else if(userAge<2){   //[0-150)
-  console.log("baby");
-} else if(userAge<6){   //[2-150)
-  console.log("kinder");
-}else if(userAge<12){   //[6-150)
-  console.log("child");
-}else if(userAge<18){   //[12-150)
-  console.log("tinager");
-}else if(userAge<65){   //[18-150)
-  console.log("adult");
-}else if(userAge<100){   //[65-150)
-  console.log("old");
-}else {   //[100-150)  if(userAge<150)
-  console.log("wow!!!");
+const sum = function (number1, number2) {
+  return number1 + number2;
+};
+const mult = function (number1, number2) {
+  return number1 * number2;
+};
+const div = function (number1, number2) {
+  return number1 / number2;
+};
+const sub = function (number1, number2) {
+  return number1 - number2;
+};
+/**
+ *
+ * @param {number} number1
+ * @param {number} number2
+ * @param {string} operator /*-+
+ * @returns {number | null}
+ */
+const calculate = function (number1 = 10, number2 = 2, operator = "+") {
+  let f = null;
+  switch (operator) {
+    case "*":
+      //return mult(number1,number2);
+      f = mult;
+      break;
+    case "/":
+      //return div(number1,number2);
+      f = div;
+      break;
+    case "+":
+      //return sum(number1,number2);
+      f = sum;
+      break;
+    case "-":
+      //return sub(number1,number2);
+      f = sub;
+      break;
+    default:
+      return null;
+  }
+  return typeof f === "function" ? f(number1, number2) : null;
+};
+
+function HOF1(word, func) {
+  func(word);
 }
-
-
-//hw
-// користувач вводить число, яке є номером дня місяця [1-31]
-// ви виводите назву дня тижня,
-// при у мові, що місяць почався з понеділка
+const HOF2 = function () {
+  return function () {
+    console.log("inner");
+  };
+};
+const f1 = HOF2();
+f1();
