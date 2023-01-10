a5c7b3ddb0267122a2b6d50631d4eb97ec1f7bab
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-10 15:42:41 +0200

    Add example loop

--- index.js
+++ index.js
@@ -1,54 +1,16 @@
-const sum = function (number1, number2) {
-  return number1 + number2;
-};
-const mult = function (number1, number2) {
-  return number1 * number2;
-};
-const div = function (number1, number2) {
-  return number1 / number2;
-};
-const sub = function (number1, number2) {
-  return number1 - number2;
-};
-/**
- *
- * @param {number} number1
- * @param {number} number2
- * @param {string} operator /*-+
- * @returns {number | null}
- */
-const calculate = function (number1 = 10, number2 = 2, operator = "+") {
-  let f = null;
-  switch (operator) {
-    case "*":
-      //return mult(number1,number2);
-      f = mult;
-      break;
-    case "/":
-      //return div(number1,number2);
-      f = div;
-      break;
-    case "+":
-      //return sum(number1,number2);
-      f = sum;
-      break;
-    case "-":
-      //return sub(number1,number2);
-      f = sub;
-      break;
-    default:
-      return null;
-  }
-  return typeof f === "function" ? f(number1, number2) : null;
-};
-
-function HOF1(word, func) {
-  func(word);
+//debugger
+let count = 1;
+while (true) {
+  console.log(count++);
+  if (count >= 5) break;
 }
-const HOF2 = function () {
-  return function () {
-    console.log("inner");
-  };
-};
-const f1 = HOF2();
-f1();
+
+const count1 = 15;
+do {
+  console.log(123123123)
+} while (count1>20);
+
+
+for (let i = 0; i < 5; i++) {
+  console.log(i)
+}
