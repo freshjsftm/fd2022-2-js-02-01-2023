9654d0956e6ba1893de72ea0533d524cdb078ec0
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-11 13:04:12 +0200

    Add calcFactorial

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-let count = 0;
-// while (true) {
-//   if (count % 2 === 0) {
-//     console.log(count);
-//   }
-//   count++;
-//   if(count>20)break;
-// }
-//debugger
-while (true) {
-  if (count % 2) {
-    count++;
-    continue;
+
+// 7! = 1*2*3*4*5*6*7
+/**
+ * 
+ * @param {number} number 
+ * @returns {number | BigInt | null}
+ */
+const calcFactorial = function(number=5){
+  if(number<0){
+    return null;
   }
-  console.log(count++);
-  if (count > 6) break;
+  if(number===0 || number===1){
+    return 1;
+  }
+  let result = 1n;
+  for(let i=1n; i<=number; i++){
+    result *= i;
+  }
+  return result;
 }
 
-for (let i = 100; i <= 600; i++) {
-  if (i % 2) continue;
-  console.log(i);
-}
-
-for (let i = 0; i <= 6; i+=2) {
-  console.log(i);
-}
+console.log(calcFactorial(1)) //120
+console.log(calcFactorial(3)) //6
+console.log(calcFactorial(25)) 
