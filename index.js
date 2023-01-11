fe61b77c7c33aba55331d26516f4daa31c512da0
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-11 14:32:10 +0200

    Add example create object

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
+const user2 = new Object();
+const user3 = Object();
 
-// 7! = 1*2*3*4*5*6*7
-/**
- * 
- * @param {number} number 
- * @returns {number | BigInt | null}
- */
-const calcFactorial = function(number=5){
-  if(number<0){
-    return null;
-  }
-  if(number===0 || number===1){
-    return 1;
-  }
-  let result = 1n;
-  for(let i=1n; i<=number; i++){
-    result *= i;
-  }
-  return result;
-}
+//literal синтаксичний цукор
+const user1 = {
+  fname: "Brad",
+  lname: "Pitt",
+  color: "white",
+  age: 59,
+  isMale: true,
+  say: function () {
+    console.log("hi!");
+  },
+  walk: function () {
+    console.log("i'm walk");
+  },
+};
 
-console.log(calcFactorial(1)) //120
-console.log(calcFactorial(3)) //6
-console.log(calcFactorial(25)) 
+//alert(user1);
+console.log(user1);
+console.log(user1.fname);
+user1.age++;
+console.log(user1.age);
+user1.fname = "Braddly";
+delete user1.color;
+console.log(user1);
+user1.hasPet = undefined;
+console.log(user1);
+user1.say();
+user1.walk();
