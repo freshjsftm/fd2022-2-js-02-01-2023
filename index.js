07f1912e9b33d006f943fe0c2cb7fc84bbdb0f52
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-11 15:36:58 +0200

    Add function constructor

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-const user2 = new Object();
-const user3 = Object();
-
-//literal синтаксичний цукор
-const user1 = {
-  fname: "Brad",
-  lname: "Pitt",
-  color: "white",
-  age: 59,
-  isMale: true,
-  say: function () {
-    console.log("hi!");
-  },
-  walk: function () {
-    console.log("i'm walk");
+const product = {
+  name: "milk",
+  price: 55,
+  currency: "uah",
+  isAvailable: true,
+  showInfo: function () {
+    console.log(this.name,':', this.price, this.currency);
   },
 };
+product.showInfo();
 
-//alert(user1);
-console.log(user1);
-console.log(user1.fname);
-user1.age++;
-console.log(user1.age);
-user1.fname = "Braddly";
-delete user1.color;
-console.log(user1);
-user1.hasPet = undefined;
-console.log(user1);
-user1.say();
-user1.walk();
+function Product( name = "test", price = 0, currency = "uah", isAvailable = false) {
+  this.name = name;
+  this.price = price;
+  this.currency = currency;
+  this.isAvailable = isAvailable;
+  this.showInfo = function(){
+    return this.name+':'+this.price+this.currency;
+  }
+}
+//debugger
+const product1 = new Product("milk", 55, undefined, true);
+const product2 = new Product("egg", 5.5);
+const product3 = new Product();
+
+console.log(product1);
+console.log(product2);
+console.log(product3);
