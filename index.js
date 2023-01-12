00e21efe0b5d20bc073aa083329791218a87d0d0
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-12 15:34:36 +0200

    Add example ``

--- index.js
+++ index.js
@@ -1,48 +1,14 @@
-function Auto(name, maxSpeed) {
-  this.name = name;
-  this.maxSpeed = maxSpeed;
-  this.currentSpeed = 0;
-  /**
-   * 
-   * @returns {number}
-   */
-  this.stop = function () {
-    return (this.currentSpeed = 0);
-  };
-  /**
-   * 
-   * @param {number} value 
-   * @returns {number | false} 
-   */
-  this.accelerate = function (value=0) {
-    if (value < 0) {
-      return false;
-    }
-    this.currentSpeed += value;
-    if (this.currentSpeed >= this.maxSpeed) {
-      return (this.currentSpeed = this.maxSpeed);
-    }
-    return this.currentSpeed;
-  };
-  /**
-   * 
-   * @param {number} value 
-   * @returns {number | false} 
-   */
-  this.deaccelerate = function (value=0) {
-    if (value < 0) {
-      return false;
-    }
-    this.currentSpeed -= value;
-    if (this.currentSpeed <= 0) {
-      return (this.currentSpeed = 0);
-    }
-    return this.currentSpeed;
-  };
-}
 
-const car = new Auto("bmw", 270);
-console.log(car.accelerate(30));
-console.log(car.accelerate(350));
-console.log(car.deaccelerate(50));
-console.log(car.deaccelerate(500));
+const str1 = `qwe
+      qwe
+queueMicrotaskqwe
+   queueMicrotaskqweeqe`;
+//alert(str1)
+console.log(str1);
+
+const number1 = 5;
+const number2 = 2;
+const operator = '*';
+const example = `${number1} ${operator} ${number2} = ${number1*number2}`;
+
+console.log(example);
