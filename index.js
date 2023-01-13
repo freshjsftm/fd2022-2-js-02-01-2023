e5153c18ec837ae20cb4e479ed1d2f898b87a2fc
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-13 16:26:20 +0200

    Change __proto__  -> prototype

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
+// const catsProto = {
+//   toString() {
+//     return "a cat named " + this.name;
+//   },
+//   sleep() {
+//     return this.name + " cat sleeping";
+//   },
+// };
 
-const catsProto = {
-  toString() {
+function CatsPrototype(){
+  this.toString = function(){
     return "a cat named " + this.name;
-  },
-  sleep() {
+  }
+  this.sleep = function(){
     return this.name + " cat sleeping";
-  },
-};
+  }
+}
 
-const cat1 = {
-  name: "Barsik",
-  __proto__: catsProto,
-};
+function Cat(name){
+  this.name = name;
+}
 
-const cat2 = {
-  name: "Pushok",
-};
-cat2.__proto__ = catsProto;
+Cat.prototype = new CatsPrototype();
 
-console.log(cat1);
-console.log(cat2);
+
+// const cat1 = {
+//   name: "Barsik",
+//   __proto__: catsProto,
+// };
+
+// const cat2 = {
+//   name: "Pushok",
+// };
+// cat2.__proto__ = catsProto;
+
+// console.log(cat1);
+// console.log(cat2);
