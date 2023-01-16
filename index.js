73d7766958c0df8b60bce29dfa8dd21a7b73468f
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-16 14:37:10 +0200

    Add example used arguments

--- index.js
+++ index.js
@@ -1,37 +1,11 @@
-// const catsProto = {
-//   toString() {
-//     return "a cat named " + this.name;
-//   },
-//   sleep() {
-//     return this.name + " cat sleeping";
-//   },
-// };
 
-function CatsPrototype(){
-  this.toString = function(){
-    return "a cat named " + this.name;
+function sum(){
+  //console.log("arguments = ", arguments)
+  let summa = 0;
+  for (let index = 0; index < arguments.length; index++) {
+    summa += arguments[index];
   }
-  this.sleep = function(){
-    return this.name + " cat sleeping";
-  }
+  return summa;
 }
 
-function Cat(name){
-  this.name = name;
-}
-
-Cat.prototype = new CatsPrototype();
-
-
-// const cat1 = {
-//   name: "Barsik",
-//   __proto__: catsProto,
-// };
-
-// const cat2 = {
-//   name: "Pushok",
-// };
-// cat2.__proto__ = catsProto;
-
-// console.log(cat1);
-// console.log(cat2);
+console.log(sum(1,2,3,7))
