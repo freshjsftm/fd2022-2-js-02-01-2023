ed7e0370fdd916f8bc627ffb02bdbd2800626ed5
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-16 16:48:38 +0200

    Add example used methods array

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
+const arr1 = [10, 20, 30, 40, 50, 60];
+const arr2 = [1, 1, 1, 5];
+const summa = arr2.reduce(function (accumulator, currentValue, index) {
+  return accumulator + (index % 2 ? currentValue : 0);
+}, 1000);
+console.log(summa);
+//const arr3 = arr1.concat(12,4,5)
+//const arr3 = arr1.slice(2, -2)
+//const key = 3;
+// console.log(arr1);
+// const arr3 = arr1.splice(2, 0, 77, 78, 79, 80);
+// console.log(arr1);
+// console.log(arr3);
+// console.log(arr1.indexOf(key, 3));
+// console.log(arr1.lastIndexOf(key));
+// console.log(arr1.includes(key));
 
-function sum(){
-  let summa = 0;
-  for (let index = 0; index < arguments.length; index++) {
-    summa += arguments[index];
-  }
-  return summa;
+//написати функцію, яка буде приймати масив та рядок
+//і повертати true, якщо рядок є в масиві
+//і false, якщо рядку в масиві не має
+//врахувати, що масив може бути порожнім!
+
+/**
+ *
+ * @param {[string]} arr
+ * @param {string} str
+ * @returns {boolean}
+ */
+function hasElement(arr, str) {
+  return arr.includes(str);
 }
 
-console.log(sum(1,2,3,7))
+//console.log(hasElement(["qwe"], "qwe"));
+
+//написати функцію, яка приймає масив з числами,
+//повертає середнє значення усіх елементів масива
+// [2,4] => 3
+//врахувати, що масив може бути порожнім - повертати null
+
+/**
+ *
+ * @param {[number]} arr
+ * @returns {number | null}
+ */
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return null;
+  }
+  // let summa = 0;
+  // for (let i = 0; i < arr.length; i++) {
+  //   summa = summa + arr[i];
+  // }
+  const summa = arr.reduce(function (resAccum, elem) {
+    return resAccum + elem;
+  });
+  return summa / arr.length;
+}
+
