8d623355a6a1a615003f36e6388ef88dc0057db8
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-19 15:03:37 +0200

    Add ways to convey context

--- index.js
+++ index.js
@@ -1,66 +1,50 @@
-function User(firstName, lastName, age, isMale, email, isSubscribe = false) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.isMale = isMale;
-  this.email = email;
-  this.isSubscribe = isSubscribe;
-}
-function UserPrototype() {
-  this.getFullName = function () {
-    return `${this.firstName} ${this.lastName}`;
-  };
-}
-User.prototype = new UserPrototype();
-
-function createUsers(amount = 1) {
-  const db = [];
-  for (let i = 0; i < amount; i++) {
-    const user = new User(
-      `firstName${i + 1}`,
-      `lastName${i + 1}`,
-      getRandomArbitrary(18, 65),
-      Math.random() > 0.5,
-      `email${i + 1}@gmail.com`
-    );
-    db.push(user);
+'use strict';
+//1 - arrow
+const site = {
+  title: 'my site',
+  headers:['Header1', 'About', 'Contact', 'Section title'],
+  showHeaders(){
+    this.headers.forEach((header)=>{
+      console.log(this.title, '|', header)
+    })
   }
-  return db;
 }
-function getRandomArbitrary(min, max) {
-  return Math.round(Math.random() * (max - min + 1) + min);
-}
+site.showHeaders();
 
-const CHECK_AGE = 35;
-const users = createUsers(100);
-//отримати масив юзерів, які будуть старші за CHECK_AGE
-const oldestUsers = users.filter(function(user){return user.age>CHECK_AGE})
-//зарандомити підписку у користувачів
-users.forEach(function(user){user.isSubscribe = Math.random()>0.5});
-console.table(users);
+//2 - this in variable
+// const site = {
+//   title: 'my site',
+//   headers:['Header1', 'About', 'Contact', 'Section title'],
+//   showHeaders(){
+//     const self = this;
+//     this.headers.forEach(function(header){
+//       console.log(self.title, '|', header)
+//     })
+//   }
+// }
 
-//отримати масив пошт користувачів жіночої статі 
-//які молодше CHECK_AGE і мають підписку
-const usersEmail = users.filter(function(user){
-  return user.isMale===false && user.isSubscribe && user.age<CHECK_AGE
-}).map(function(user){
-  return user.email;
-})
-console.table(usersEmail);
+//thisArgs in methods array
+// const site = {
+//   title: "my site",
+//   headers: ["Header1", "About", "Contact", "Section title"],
+//   showHeaders() {
+//     this.headers.forEach(logHeader, this);
+//   },
+// };
+// function logHeader(header) {
+//   console.log(this.title, "|", header);
+// }
 
+// const site = {
+//   title: "my site",
+//   headers: ["Header1", "About", "Contact", "Section title"],
+//   showHeaders() {
+//     this.headers.forEach(logHeader.bind(this));
+//   },
+// };
 
-//отримати массив повних імен користувачів
-const fullNameUserArray = users.map(function(user){return user.getFullName()});
-//console.log(fullNameUserArray)
+// site.showHeaders();
 
-//порахувати кількість жінок/чоловіків
-console.log('male = ',
-  users.filter(function (user) {
-    return user.isMale;
-  }).length
-);
-console.log('female = ',
-  users.filter(function (user) {
-    return !user.isMale;
-  }).length
-);
+// function logHeader(header){
+//   console.log(this.title, '|', header)
+// }
