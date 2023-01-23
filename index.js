e253af3118dc22198f8937ae570572515f5878fb
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-23 14:57:58 +0200

    Add new Number

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 "use strict";
 
-const check = Boolean('');
-console.log(check);
-const boolean = new Boolean(false);
-//console.log(boolean.valueOf());
+const number = new Number(0);
 
-if (boolean) {
-  console.log("block true");
-  console.log(boolean.valueOf());
-} else {
-  console.log("false");
+if(number){
+  console.log(number)
 }
+console.log(number.toExponential());
+
+console.log((12345.12345).toPrecision(7));
+console.log((12345.12345).toFixed(7));
+
+const num = 789.789456;
+console.log(num);
+new Number(num);
+console.log(num.toPrecision(7));
+console.log(num.toFixed(2));
