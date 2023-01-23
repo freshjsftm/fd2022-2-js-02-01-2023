aec1052dde6aff5399b8d039d26e0bf85b9fa469
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-23 16:49:56 +0200

    Add toJadenCase

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 "use strict";
 
-const userName = "        brad        ";
-// console.log(userName.toUpperCase())
-const userNameUpper = userName.trim().toUpperCase().repeat(3);
-console.log(userNameUpper);
+//написати функцію, яка буде приймати рядок і форматувати його до Jaden Case і повертати новий форматований рядок
+//'To Be Or Not To Be'
 
-const arr = [1, 2, 3, 4, 5];
-const strArr = arr.join('-');
-console.log(strArr);
+// function toJadenCase(str) {
+//   //перевести увесь рядок в маленькі букви toLowerCase
+//   const newStr = str.trim().toLowerCase();
+//   //розібрати рядок на окремі слова split - отримати масив слів
+//   const words = newStr.split(' ');
+//   //для кожного слова зробити першу літеру велику, а наступні маленькі
+//   const newWords = words.map((word)=>{
+//     return word[0].toUpperCase()+ word.slice(1);
+//   })
+//   //з масиву зробити знову рядок join
+//   const readyStr = newWords.join(' ');
+//   //повернути цей рядок
+//   return readyStr;
+// }
+const str = "    tO    bE OR    nOT to    Be     ";
 
-const p = 'The quick brown fox jumps over the lazy dog.';
-if(p.includes('.')){
-  const posDote = p.indexOf('.');
-  const pArr = p.substring(0,posDote).split(' ');
-  console.log(pArr);
+function toJadenCase(str, separator = " ") {
+  return str
+    .trim()
+    .toLowerCase()
+    .split(separator) //array
+    .filter((word) => word !== "")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(separator);
 }
+console.log(toJadenCase(str));
