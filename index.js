4ac364989e91c51e45b657e377864ea5f67946a4
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-24 14:30:22 +0200

    Add example old syntax

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
 "use strict";
 
-//написати функцію, яка буде приймати рядок і форматувати його до Jaden Case і повертати новий форматований рядок
-//'To Be Or Not To Be'
-
-// function toJadenCase(str) {
-//   //перевести увесь рядок в маленькі букви toLowerCase
-//   const newStr = str.trim().toLowerCase();
-//   //розібрати рядок на окремі слова split - отримати масив слів
-//   const words = newStr.split(' ');
-//   //для кожного слова зробити першу літеру велику, а наступні маленькі
-//   const newWords = words.map((word)=>{
-//     return word[0].toUpperCase()+ word.slice(1);
-//   })
-//   //з масиву зробити знову рядок join
-//   const readyStr = newWords.join(' ');
-//   //повернути цей рядок
-//   return readyStr;
-// }
-const str = "    tO    bE OR    nOT to    Be     ";
-
-function toJadenCase(str, separator = " ") {
-  return str
-    .trim()
-    .toLowerCase()
-    .split(separator) //array
-    .filter((word) => word !== "")
-    .map((word) => word[0].toUpperCase() + word.slice(1))
-    .join(separator);
-}
-console.log(toJadenCase(str));
+const user = new User('Brad', 'Pitt', 59);
