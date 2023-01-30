f312798d760cf4fe1c1dda57712992c1ff57c0d4
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-01-30 12:37:28 +0200

    Add example pattern AbstractFactory

--- index.js
+++ index.js
@@ -1,132 +1,66 @@
 "use strict";
-/*
-  інкапсуляція (абстракція)
-  успадкування
-  поліморфізм
-*/
 
-function logPerimetrFigure(figure){
-  if(figure instanceof Figure){
-    console.log(figure.name, ' perimetr = ', figure.getPerimetr())
-    return;
-  }
-  throw new TypeError('instance must be extends Figure')
+class Button{
+  constructor(){}
+  createButton(){}
 }
-
-class Figure {
-  constructor(name) {
-    if(this.constructor === Figure){
-      throw new Error('Not create instance from Figure!')
-    }
-    this.name = name;
+class WinButton extends Button{
+  constructor(){super()}
+  createButton(){
+    //realization at win
   }
-  getPerimetr() {}
-  getArea() {}
 }
-
-class Circle extends Figure {
-  constructor(radius) {
-    super("circle");
-    this.radius = radius;
+class UnixButton extends Button{
+  constructor(){super()}
+  createButton(){
+    //realization at unix
   }
-  get radius() {
-    return this._radius;
-  }
-  set radius(value) {
-    if (typeof value !== "number") {
-      throw new TypeError("radius must be number");
-    }
-    if (value <= 0) {
-      throw new RangeError("radius must be positive");
-    }
-    this._radius = value;
-  }
-  getPerimetr() {
-    return 2 * Math.PI * this._radius;
-  }
-  getArea() {
-    return Math.PI * this._radius * this._radius;
+}
+class MacButton extends Button{
+  constructor(){super()}
+  createButton(){
+    //realization at mac
   }
 }
 
-class Square extends Figure {
-  constructor(side) {
-    super("square");
-    this.side = side;
+class OSFactory{
+  createButton(){}
+  createPanel(){}
+}
+class WinOSFactory extends OSFactory{
+  createButton(){
+    return new WinButton()
   }
-  get side() {
-    return this._side;
+  createPanel(){}
+}
+class UnixOSFactory extends OSFactory{
+  createButton(){
+    return new UnixButton()
   }
-  set side(value) {
-    if (typeof value !== "number") {
-      throw new TypeError("side must be number");
-    }
-    if (value <= 0) {
-      throw new RangeError("side must be positive");
-    }
-    this._side = value;
+  createPanel(){}
+}
+class MacOSFactory extends OSFactory{
+  createButton(){
+    return new MacButton()
   }
-  getPerimetr() {
-    return 4 * this._side;
-  }
-  getArea() {
-    return this._side * this._side;
-  }
+  createPanel(){}
 }
 
-class Triangular extends Figure {
-  constructor(a, b, c) {
-    super("triangular");
-    this.setSides(a, b, c); 
-  }
-  setSides(a, b, c) {
-    if (
-      typeof a !== "number" ||
-      typeof b !== "number" ||
-      typeof c !== "number"
-    ) {
-      throw new TypeError("side must be number");
+class Studio{
+  render(OS){   
+    switch(OS){
+      case 'win':{
+        this._factory = new WinOSFactory();
+      }
+      case 'unix':{
+        this._factory = new UnixOSFactory();
+      }
+      case 'mac':{
+        this._factory = new MacOSFactory();
+      }
+      default: break;
     }
-    if (a <= 0 || b <= 0 || c <= 0) {
-      throw new RangeError("side must be positive");
-    }
-    if (a + b > c && b + c > a && a + c > b) {
-      this._a = a;
-      this._b = b;
-      this._c = c;
-      return;
-    }
-    throw new RangeError("side must be right");
-  }
-  get a() {
-    return this._a;
-  }
-  get b() {
-    return this._b;
-  }
-  get c() {
-    return this._c;
-  }
-  getPerimetr() {
-    return this._a+this._b+this._c;
-  }
-  getArea() {
-    //formula Gerona
+    const button = this._factory.createButton();
+    const panel = this._factory.createPanel();
   }
 }
-
-// const figure0 = new Figure('figure');
-// console.log(figure0.getPerimetr());
-
-const figure1 = new Circle(10);
-figure1.radius += 2;
-
-const figure2 = new Square(10);
-figure2.side += 2;
-
-const figure3 = new Triangular(3,4,5);
-
-
-logPerimetrFigure(figure1)
-logPerimetrFigure(figure2)
-logPerimetrFigure(figure3)
