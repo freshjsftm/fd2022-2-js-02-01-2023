b73da01bf0ee3b5b748650e1d21b6ee383534043
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-01 16:57:53 +0200

    Add checkExpression

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
 "use strict";
+const options = {
+  breckets: {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "<": ">",
+    "<<": ">>",
+  },
+};
+/**
+ *
+ * @param {string} str
+ * @param {object} options
+ * @param {object} options.breckets
+ * @returns
+ */
+const checkExpression = (str, options) => {
+  const stack = new Stack(str.length);
+  const closesBreckets = Object.values(options.breckets);
+  for (const symbol of str) {
+    if (options.breckets[symbol]) {
+      stack.push(symbol); 
+      continue;
+    }
+    const lastSymbolInStack = stack.pick();   
+    const correctSymbol = options.breckets[lastSymbolInStack]; 
+    if (symbol === correctSymbol) {
+      stack.pop();
+    } else if(closesBreckets.includes(symbol)){
+      return false;
+    }
+  }
+  return stack.isEmpty;
+};
+console.log(checkExpression("(2)", options));
+console.log(checkExpression("[]])(<>{}))", options));
+console.log(checkExpression(" ( 2[(*)]3 + {a[0]}) ", options));
 
+
+
+const queue = new Queue(1,5,9);
+console.log(queue)
+console.log(queue.deQueue())
+console.log(queue.deQueue())
+console.log(queue.enQueue(77))
+console.log(queue)
