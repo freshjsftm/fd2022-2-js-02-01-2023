af239ccc4e7fb658f85ba0641a73f2fa3ec1d4ab
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-02-02 14:34:06 +0200

    Add example new Map()

--- index.js
+++ index.js
@@ -1,47 +1,19 @@
 "use strict";
-const options = {
-  breckets: {
-    "(": ")",
-    "[": "]",
-    "{": "}",
-    "<": ">",
-    "<<": ">>",
-  },
-};
-/**
- *
- * @param {string} str
- * @param {object} options
- * @param {object} options.breckets
- * @returns
- */
-const checkExpression = (str, options) => {
-  const stack = new Stack(str.length);
-  const closesBreckets = Object.values(options.breckets);
-  for (const symbol of str) {
-    if (options.breckets[symbol]) {
-      stack.push(symbol); 
-      continue;
-    }
-    const lastSymbolInStack = stack.pick();   
-    const correctSymbol = options.breckets[lastSymbolInStack]; 
-    if (symbol === correctSymbol) {
-      stack.pop();
-    } else if(closesBreckets.includes(symbol)){
-      return false;
-    }
-  }
-  return stack.isEmpty;
-};
-console.log(checkExpression("(2)", options));
-console.log(checkExpression("[]])(<>{}))", options));
-console.log(checkExpression(" ( 2[(*)]3 + {a[0]}) ", options));
 
+const obj={
+  prop:12,
+  1:12,
+}
+const arr = []
+const f = ()=>{}
 
-
-const queue = new Queue(1,5,9);
-console.log(queue)
-console.log(queue.deQueue())
-console.log(queue.deQueue())
-console.log(queue.enQueue(77))
-console.log(queue)
+const map1 = new Map();
+map1.set(true, 'true');
+map1.set(obj, 'obj');
+map1.set(arr, 'arr');
+map1.set(1, 10000);
+map1.set(f, 'func');
+console.log(map1);
+console.log(map1.get(1));
+map1.delete(1)
+console.log(map1.has(1));
